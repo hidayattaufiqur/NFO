b3cf27e7ebe2ce19e85d671bfceead59bfdf44cf
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-06-23 19:55:07 +0700

    feat(decorator): require_authorization custom decorator now supports coroutine (async)

--- app/utils/decorator.py
+++ app/utils/decorator.py
@@ -1,14 +1,20 @@
 from functools import wraps
 from flask import jsonify
+from inspect import iscoroutinefunction
 
 from .auth import is_authorized, refresh_session
 
 def require_authorization(f):
     @wraps(f)
-    def decorated_function(*args, **kwargs):
+    async def decorated_function(*args, **kwargs):
         auth_response = is_authorized()
         if auth_response:
             return jsonify(auth_response), 401
         refresh_session()
-        return f(*args, **kwargs)
+        
+        if iscoroutinefunction(f):
+            return await f(*args, **kwargs)
+        else:
+            return f(*args, **kwargs)
+
     return decorated_function
