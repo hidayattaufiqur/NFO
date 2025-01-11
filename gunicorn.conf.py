bdfb58376681f1668786cd7b9dfd2f96c2923642
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2025-01-11 18:11:16 +0700

    feat(gunicorn): optimize gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,28 +1,29 @@
-import os
-import multiprocessing
-
-# Socket to bind bind = 'unix:/home/nixos-server/Fun/Projects/myproject.sock'
-
-'''
-configurations below follow Google Cloud documentation
-[https://cloud.google.com/appengine/docs/flexible/python/runtime#recommended_gunicorn_configuration]
-'''
-
-# workers = multiprocessing.cpu_count() * 2 + 1
-worker = 2
-forwarded_allow_ips = '*'
-secure_scheme_headers = {'X-FORWARDED-PROTO': 'https'}
-
-timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5000')
-
-# The maximum number of requests a worker will process before restarting
-max_requests = 1000
-
-# Logging configuration
-accesslog = '/home/nixos-server/Fun/Projects/ontology-BE/logs/gunicorn-access.log'
-errorlog = '/home/nixos-server/Fun/Projects/ontology-BE/logs/gunicorn-error.log'
-loglevel = 'info'
-
-# Daemonize the Gunicorn process (run in the background)
-# daemon = True
+# gunicorn.conf.py  
+import os  
+  
+# Determine the project root directory  
+project_root = os.path.dirname(os.path.abspath(__file__))  
+  
+# Socket to bind  
+bind = f'unix:{project_root}/gunicorn.sock'  
+  
+# Configurations based on Google Cloud documentation  
+workers = 2 
+forwarded_allow_ips = '*'  
+secure_scheme_headers = {'X-FORWARDED-PROTO': 'https'}  
+  
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))  
+  
+# The maximum number of requests a worker will process before restarting  
+max_requests = 1000  
+  
+# Logging configuration  
+log_dir = os.path.join(project_root, 'logs')  
+os.makedirs(log_dir, exist_ok=True)  # Ensure the log directory exists  
+  
+accesslog = os.path.join(log_dir, 'gunicorn-access.log')  
+errorlog = os.path.join(log_dir, 'gunicorn-error.log')  
+loglevel = 'info'  
+  
+# Daemonize the Gunicorn process (run in the background)  
+daemon = False  # Set to False to allow systemd to manage the process  
